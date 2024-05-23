cd1a2111a9daac5822525afbf3e7c0d3d45c80cd
Sonu Jain <sayra.jain555@gmail.com>
2024-05-23 20:19:50 +0000

    Added fixes of train py

--- notebooks/train.py
+++ notebooks/train.py
@@ -1,90 +1,90 @@
 
-# Import the required library 
+
+# Import the required library
 import xgboost as xgb
 import os
 import pickle
+import boto3
+import argparse
+import sagemaker
 
 model_file_name = "pipeline_model"
+
+
 
 # Main Function
 def main():
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("--model_dir",type = str , default = os.environ.get("SM_MODEL_DIR"))
-
+    parser.add_argument("--model_dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
     args, _ = parser.parse_known_args()
-    
-    # Get the current directory
-    current_dir = os.getcwd()
 
-    # Get the parent directory (one level up)
-    parent_dir = os.path.dirname(current_dir)
+    # Specify the AWS region
+    region_name = 'us-east-1'  # Change this to your desired region
     
-    # Print the parent directory
-    print("Parent Directory:", parent_dir)
+    # Create a SageMaker session with the specified region
+    session = boto3.Session(region_name=region_name)
+    sagemaker_session = sagemaker.Session(boto_session=session)
+    bucket = sagemaker_session.default_bucket()
     
-    preprocessed_data_dir = parent_dir+'/datasets/'
-    model_dir = parent_dir+'/models/'
+    # Initialize the S3 client
+    s3 = boto3.client('s3')
     
-    # Load X_test from file
-    with open(os.path.join(preprocessed_data_dir,'resnet_X_train.pkl'), 'rb') as f:
-        X_train = pickle.load(f)
-        
-    # Load y_test from file
-    with open(os.path.join(preprocessed_data_dir,'resnety_train.pkl'), 'rb') as f:
-        y_train = pickle.load(f)
+    # Specify the bucket name and key (path) of the pickle file
+    bucket_name = bucket
+    X_train_file_key = "datasets/resnet_X_train.pkl"
+    y_train_file_key = "datasets/resnety_train.pkl"
+    X_test_file_key = "datasets/resnetX_test.pkl"
+    y_test_file_key = "datasets/resnety_test.pkl"
+
+    def loadData(file_key):
+        # Read the pickle file from S3
+        response = s3.get_object(Bucket=bucket_name, Key=file_key)
+        pickle_bytes = response['Body'].read()
+        # Load the pickle file from bytes
+        data = pickle.loads(pickle_bytes)
     
-    # Creating a copy of labels
-    y_train_1 = y_train.copy()
+        return data
+
+    X_train = loadData(X_train_file_key)
+    y_train =  loadData(y_train_file_key)
+    X_test =  loadData(X_test_file_key)
+    y_test =  loadData(y_test_file_key)
     
     # Replace all occurrences defective as 0 and good as 1
     for i in range(len(y_train)):
-        if y_train[i]=='defective' : 
+        if y_train[i] == 'defective': 
             y_train[i] = 0
         else:
             y_train[i] = 1
-    
     # Conert it into string to int
-    y_train=y_train.astype(int)
-    
+    y_train = y_train.astype(int)
     # Model selection
     xgb_model = xgb.XGBClassifier()
-    
     # Train the model
     xgb_model.fit(X_train, y_train)
 
-    # Load X_test from file
-    with open(os.path.join(preprocessed_data_dir,'resnetX_test.pkl'), 'rb') as f:
-        X_test = pickle.load(f)
-        
-    # Load y_test from file
-    with open(os.path.join(preprocessed_data_dir,'resnety_test.pkl'), 'rb') as f:
-        y_test = pickle.load(f)
-    
-    # Creating a copy of labels
-    y_test_1 = y_test.copy()
-    
     # Replace all occurrences defective as 0 and good as 1
     for i in range(len(y_test)):
-        if y_test[i]=='defective' : 
+        if y_test[i] == 'defective':
             y_test[i] = 0
         else:
             y_test[i] = 1
-    
     # Conert it into string to int
-    y_test=y_test.astype(int)
-
-    #train accuracy
-    train_accuracy = xgb_model.score(X_train,y_train)
+    y_test = y_test.astype(int)
+    # train accuracy
+    train_accuracy = xgb_model.score(X_train, y_train)
+    # test accuracy
+    test_accuracy = xgb_model.score(X_test, y_test)
+    # Save Model
+    model_save_path = os.path.join(args.model_dir, model_file_name)
+    with open(model_save_path,'wb') as f:
+        pickle.dump(xgb_model, f)
+    print(f"Model save at path: {model_save_path}")
     print(f"Training accuracy: {train_accuracy: .4f}")
-    
-    #test accuracy
-    test_accuracy = xgb_model.score(X_test,y_test)
     print(f"Testing accuracy: {test_accuracy: .4f}")
 
-    model_save_path = os.path.join(args.model_dir,model_file_name)
-    pickle.dump(xgb_model, model_save_path)
-    print(f"Model save at path: {model_save_path}")
 
-if __name__=="main":
+# Check if the script is being executed as the main module
+if __name__ == "__main__":
+    # Call the main function
     main()
